9412104729d9c137101ad02c4d8647310ce6f2c3
AsaturHakhverdyan <hakhverdyanasatur@gmail.com>
2022-10-07 16:42:14 +0400

    Linz-Optik-Login-Page

---
+++ src/pages/login/LoginPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from '../../images/logo.svg'
+import LoginForm from './LoginForm';
+
+const LoginPageLgo: React.FC = () => {
+  return (
+    <div className='max-w-[1100px] mx-auto py-5 px-4'>
+     <div>
+      <div className='flex items-center justify-center relative'>
+          <img 
+            src={logo} 
+            alt="logo"
+            className='w-40'
+          />
+        </div>
+        <div className='border-b-2 border-blue-800'>
+          <h1 className='text-center text-[30px]  font-bold text-blue-800 md:text-[2rem] md:text-left '>LOGIN</h1>
+        </div>
+     </div>
+      <LoginForm/>
+    </div>
+  )
+};
+
+export default LoginPageLgo;
